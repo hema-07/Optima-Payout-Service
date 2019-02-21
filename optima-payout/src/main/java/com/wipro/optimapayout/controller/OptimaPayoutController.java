64d01d5a975d04a0d70cd3cf3ebc9b2376b5b9cc
Hemavathi <hemavathi.tamilmaran95@gmail.com>
2019-02-21 23:28:56 +0530

    payout - last commit

--- optima-payout/src/main/java/com/wipro/optimapayout/controller/OptimaPayoutController.java
+++ optima-payout/src/main/java/com/wipro/optimapayout/controller/OptimaPayoutController.java
@@ -1,6 +1,5 @@
 package com.wipro.optimapayout.controller;
 
-
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -22,8 +21,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value="/payOutPlanService")
-public class OptimaPayoutController {
-	
+public class PayoutController {
+
 	@RequestMapping(value="/login")
 	public String login() {
 		return "login";
@@ -34,8 +33,10 @@ public class OptimaPayoutController {
 		Object obj;
 		 Map<Double, List<Double>> debitMap = new HashMap<Double, List<Double>>();
 		 Map<Double, List<Double>> creditMap = new HashMap<Double, List<Double>>();
-		 Map<Double, List<Double>> creditresult = new HashMap<Double, List<Double>>();
+		 Map<Double, List<Double>> creditDuplicate = new HashMap<Double, List<Double>>();
 		 ArrayList<Double> funds = new ArrayList<Double>();
+		 ArrayList<Double> outstanding = new ArrayList<Double>();
+		 ArrayList<Double> mindue = new ArrayList<Double>();
 		 try {
 			//-----------------debit----------------------------------------
 			obj = new JSONParser().parse(new FileReader("src/main/resources/data.json"));
@@ -69,14 +70,13 @@ public class OptimaPayoutController {
 				l.add(out);
 				l.add(due);
 				totalCreditBal = totalCreditBal + out;
-				creditresult.put(Double.parseDouble((String) json.get("APR")), l);
+				creditDuplicate.put(Double.parseDouble((String) json.get("APR")), l);
 				l = null;
 			}
-			totalCreditBal=0.00;
-			JSONArray credit = (JSONArray) jo.get("creditDetails");
-			Iterator i2 = credit.iterator();
-			while (i2.hasNext()) {
-				JSONObject json = (JSONObject) i2.next();
+			JSONArray credit2 = (JSONArray) jo.get("creditDetails");
+			Iterator i4 = credit2.iterator();
+			while (i4.hasNext()) {
+				JSONObject json = (JSONObject) i4.next();
 				List<Double> l = new ArrayList<Double>();
 				double due = Double.parseDouble((String) json.get("minDue"));
 				double out = Double.parseDouble((String) json.get("Outstanding"));
@@ -87,144 +87,101 @@ public class OptimaPayoutController {
 				l = null;
 			}
 
-			Map<Double, List<Double>> creditresult1 = new TreeMap<Double, List<Double>>(Collections.reverseOrder());
-			creditresult1.putAll(creditresult);
-			model.addAttribute("c",creditresult1);
-
 			//-----------------debit sort by AER----------------------------------------
 			
 			Map<Double, List<Double>> debitMapSort = new TreeMap<Double, List<Double>>(debitMap);
 			System.out.println(debitMapSort);
 			model.addAttribute("d",debitMapSort);
+
 			
 			//-------------------credit sort by APR----------------------------------------
 			
 			Map<Double, List<Double>> creditMapSort = new TreeMap<Double, List<Double>>(Collections.reverseOrder());
 			creditMapSort.putAll(creditMap);
 			System.out.println(creditMapSort);
+			model.addAttribute("c", creditMapSort);
+			Map<Double, List<Double>> creditMapSortResult = new TreeMap<Double, List<Double>>(Collections.reverseOrder());
+			creditMapSortResult.putAll(creditDuplicate);
+			System.out.println(creditMapSortResult);
 			
 			//---------------------logic-----------------------------------------------
-			if(totalDebitBal >= totalCreditBal) {
-				System.out.println("scen1 d>c total debit balance :"+totalDebitBal+"total credit bal :"+totalCreditBal);
 				Iterator<Map.Entry<Double, List<Double>>> itr = debitMapSort.entrySet().iterator(); 
-				Iterator<Map.Entry<Double, List<Double>>> itr1 = creditMapSort.entrySet().iterator();
-				 while(itr.hasNext()) 
-			        { 
+				Iterator<Map.Entry<Double, List<Double>>> itr1 = creditMapSortResult.entrySet().iterator();
+				
+				 while(itr.hasNext())  { 
 			             Entry<Double, List<Double>> entry = itr.next(); 
 			             funds.add(entry.getValue().get(3));
-			             
 			        } 
-				 int i=0;
-					
-					 while(itr1.hasNext()) {
-						 Entry<Double, List<Double>> entry = itr1.next();
-						 while (entry.getValue().get(0) != 0) {
-				
-//							System.out.println(funds.get(i)+"  "+entry.getValue().get(0));
-							
-							if(funds.get(i) > 0 && entry.getValue().get(0) > 0.00){
-								if(funds.get(i) >= entry.getValue().get(0)) {
-									funds.set(i, funds.get(i)-entry.getValue().get(0));
-									entry.getValue().set(0, 0.00);
-									entry.getValue().set(1, 0.00);
-//									System.out.println(i+"if  :"+funds.get(i)+"  "+entry.getValue().get(0));
-								}
-								else {
-									entry.getValue().set(0, entry.getValue().get(0)-funds.get(i));
-									entry.getValue().set(1, 0.00);
-									funds.set(i, 0.00);
-//									System.out.println(i+"else  :"+funds.get(i)+"  "+entry.getValue().get(0));
-								}
-							}
-							if (funds.get(i) == 0) {
-								i++;
-							}
-//							System.out.println(i);
-								
-						 }
-					 }
-					 int k=0;
-					 System.out.println("itr" + itr.hasNext());
-					 Iterator<Map.Entry<Double, List<Double>>> fundBal = debitMapSort.entrySet().iterator();
-						while(fundBal.hasNext()) {
-							Entry<Double, List<Double>> entry = fundBal.next();
-								System.out.println("funds"+funds.get(k)+" "+entry.getValue().get(1)+" "+entry.getValue().get(2));
-								funds.set(k, funds.get(k)+entry.getValue().get(1)+entry.getValue().get(2));
-								System.out.println(funds);
-							k++;
-						}
-					model.addAttribute("creditresult", creditMapSort);
-					System.out.println(funds);
-					System.out.println(creditMapSort);
-					model.addAttribute("funds", funds);
-			} //-----if end-------------
-			else {
-				System.out.println("sc 2 c>d total debit balance :"+totalDebitBal+"total credit bal :"+totalCreditBal);
-				Iterator<Map.Entry<Double, List<Double>>> itr = debitMapSort.entrySet().iterator(); 
-				Iterator<Map.Entry<Double, List<Double>>> itr1 = creditMapSort.entrySet().iterator();
-				
-				 while(itr.hasNext()) 
-			        { 
-			             Entry<Double, List<Double>> entry = itr.next(); 
-			             funds.add(entry.getValue().get(3));
-			             
-			        } 
-				 int i=0;
 				 while(itr1.hasNext()) {
-					Entry<Double, List<Double>> entry = itr1.next();
-					if(funds.get(i) >= entry.getValue().get(1)) {
-						funds.set(i, funds.get(i)-entry.getValue().get(1));
-						entry.getValue().set(0, entry.getValue().get(0)-entry.getValue().get(1));
-						entry.getValue().set(1, 0.00);
-						
-					}
-					else {
-						funds.set(i, entry.getValue().get(1)-funds.get(i));
-						entry.getValue().set(0, funds.get(i)-entry.getValue().get(1));
-						entry.getValue().set(1, 0.00);
-					}i++;
-					if(i >= funds.size()) {
-						i=0;
-					}
-					System.out.println(funds);
-					for(int j=0;j<funds.size()-1;j++) {
-						System.out.println(funds.get(i));
-							if(funds.get(j) >= entry.getValue().get(0)) {
-								double a = funds.get(j)-entry.getValue().get(0);
-								funds.set(j, a);
-								entry.getValue().set(0, 0.00);
-								System.out.println("a"+a);
-								a=0;
+					 	Entry<Double, List<Double>> entry = itr1.next();
+					 	outstanding.add(entry.getValue().get(0));
+					 	mindue.add(entry.getValue().get(1));
+				 }
+				 System.out.println("mindue"+mindue);
+			 		System.out.println("funds"+funds);
+			 		System.out.println("outstanding"+outstanding);
+				 for(int c=0;c<mindue.size();c++) {
+				 		for(int d=0;d<funds.size();d++) {
+				 			if(funds.get(d) >= mindue.get(c)) {
+				 				System.out.println("Before If" + funds.get(d)+" "+mindue.get(c)+" "+d+" "+c);
+				 				funds.set(d, funds.get(d)-mindue.get(c));
+								outstanding.set(c, outstanding.get(c)-mindue.get(c));
+								mindue.set(c, 0.00);
+								System.out.println("After If" + funds.get(d)+" "+mindue.get(c)+" "+d+" "+c);
 							}
 							else {
-								double b = entry.getValue().get(0)-funds.get(j);
-								funds.set(j, 0.00);
-								entry.getValue().set(0, b);
-								System.out.println("b"+b);
-								b=0;
+								System.out.println("Before else" + funds.get(d)+" "+mindue.get(c)+" "+d+" "+c);
+								outstanding.set(c, outstanding.get(c)-funds.get(d));
+								mindue.set(c, mindue.get(c)-funds.get(d));
+								funds.set(d, 0.00);
+								System.out.println("After else" + funds.get(d)+" "+mindue.get(c)+" "+d+" "+c);
+
 							}
-					
-					}
+				 		}
+				 		System.out.println("mindue"+mindue);
+				 		System.out.println("funds"+funds);
+				 		System.out.println("outstanding"+outstanding);
 				 }
-				
-					System.out.println(funds);
-					System.out.println(creditMapSort);
-					
-					while(itr.hasNext()) {
-						Entry<Double, List<Double>> entry = itr.next();
-						for(int k=0;k<funds.size();k++) {
-							funds.set(k, funds.get(k)+entry.getValue().get(1)+entry.getValue().get(2));
-							System.out.println(funds);
+			 	for(int c=0;c<outstanding.size();c++) {
+			 		for(int d=0;d<funds.size();d++) {
+						System.out.println("In main if" + funds.get(d)+" "+outstanding.get(c)+"d :"+d+"c :"+c);
+						if(funds.get(d) >= outstanding.get(c)) {
+							funds.set(d, funds.get(d)-outstanding.get(c));
+							outstanding.set(c, 0.00);
+							System.out.println("In if" + funds.get(d)+" "+outstanding.get(c)+"d :"+d+"c :"+c);			
+						}
+						else {
+							System.out.println("In else before deducting" + funds.get(d)+" "+outstanding.get(c)+"d :"+d+"c :"+c);
+							outstanding.set(c, outstanding.get(c)-funds.get(d));
+							funds.set(d, 0.00);
+							System.out.println("In else after deducting" + funds.get(d)+" "+outstanding.get(c)+"d :"+d+"c :"+c);
 						}
 					}
-					model.addAttribute("funds", funds);		
-					model.addAttribute("creditresult", creditMapSort);
-			}//-------else end------------
-			
-		
-			
-		        
-			model.addAttribute("credit", creditMapSort);
+				 }
+				System.out.println("end of for funds");
+				
+					System.out.println(funds);
+					System.out.println("creditMapSortResult"+creditMapSortResult);
+					System.out.println("credit map sort"+creditMapSort);
+					System.out.println("debitmap sort"+debitMapSort);
+					
+					Iterator<Map.Entry<Double, List<Double>>> itrDebit = debitMapSort.entrySet().iterator();
+					int k=0;
+					while(itrDebit.hasNext()) {
+						Entry<Double, List<Double>> entry2 = itrDebit.next();
+							funds.set(k, funds.get(k)+entry2.getValue().get(1)+entry2.getValue().get(2));
+							k++;
+					}
+					
+					System.out.println(funds+"funds ");
+					System.out.println("creditmapsort"+creditMapSort);
+					System.out.println("outstanding"+outstanding);
+					model.addAttribute("funds", funds);	
+					model.addAttribute("out", outstanding);
+					model.addAttribute("mindue", mindue);
+					model.addAttribute("credit", creditMapSortResult);
+					
+					
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
